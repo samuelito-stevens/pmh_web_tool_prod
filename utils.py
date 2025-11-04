222da1b54a065c71dfdded2679ab5061293e1dbc
Sam Stevens <samstevens44449999@gmail.com>
2025-11-04 21:38:37 +0000

    boop

--- utils.py
+++ utils.py
@@ -225,7 +225,7 @@ def create_sliders_from_csv(df, currency_code, convert_input, conversion_rate):
                             
                             #with col1:
                             if use_currency_formatting:
-                                col1, col2 = st.columns([0.06, 0.94])
+                                col1, col2 = st.columns([0.08, 0.92])
                                 with col1:
                                     st.markdown(f"<div style='padding-top: 35.5px; padding-left: 0px; font-size: 16px;'>{currency_code if convert_input else "USD"}</div>", unsafe_allow_html=True)
                                 with col2:
